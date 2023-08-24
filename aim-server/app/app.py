9d0a45ab2e2f21619c72bea91b4b77ab819f200f
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2023-08-24 16:52:29 -0500

    Setup decorator for routes and get ready a login page data message to make sure we stablish correct connection with the frontend

--- aim-server/app/app.py
+++ aim-server/app/app.py
@@ -3,13 +3,19 @@ import os
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, jsonify
 from flask_session import Session
+from flask_cors import CORS
+
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd
+from helpers import login_required
 from datetime import datetime
 
 # Configure application
 app = Flask(__name__)
+
+# Allow requests from specific origins
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
@@ -61,7 +67,40 @@ def init_db():
         );
     """)
 
+init_db()
 
-if __name__ == "__main__":
-    init_db()
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+    login_page_data = {"text": "Welcome from the server"}
+    return jsonify(login_page_data)
 
+# API:
+@app.route("/get_scores")
+@login_required
+def get_transactions():
+    # scores = db.execute(
+    #     )
+    return NULL
+
+@app.route("/")
+@login_required
+def index():
+    """Show my scores in dashboard"""
+    return NULL
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return NULL
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
