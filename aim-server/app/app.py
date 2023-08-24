285d1fa4d2bf6d5d72da9879d6da1fb036b4a83e
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2023-08-24 13:47:38 -0500

    Configure db conecction with sql through cs50 library and initialize database tables(users, skills and scores)  within init_db function

--- aim-server/app/app.py
+++ aim-server/app/app.py
@@ -17,4 +17,51 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///aim.db")
+
+# Initialize database tables
+def init_db():
+    db.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            username TEXT NOT NULL UNIQUE,
+            password_hash TEXT NOT NULL
+        );
+    """)
+
+    db.execute("""
+        CREATE TABLE IF NOT EXISTS skills (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL
+        );
+    """)
+
+     # Skills to insert, including new skills
+    skills_to_insert = ["aim", "reaction time", "new_skill_1", "new_skill_2"]
+    
+    # Get existing skills from the database
+    existing_skills = db.execute("SELECT name FROM skills")
+
+    # Filter out existing skills from skills_to_insert
+    new_skills = [skill for skill in skills_to_insert if skill not in (row['name'] for row in existing_skills)]
+
+    # Insert new skills
+    for skill_name in new_skills:
+        db.execute("INSERT INTO skills (name) VALUES (?)", skill_name)
+
+    db.execute("""
+        CREATE TABLE IF NOT EXISTS scores (
+            id INTEGER PRIMARY KEY,
+            user_id INTEGER NOT NULL,
+            skill_id INTEGER NOT NULL,
+            score INTEGER NOT NULL,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY(user_id) REFERENCES users(id),
+            FOREIGN KEY(skill_id) REFERENCES skills(id)
+        );
+    """)
+
+
+if __name__ == "__main__":
+    init_db()
+
